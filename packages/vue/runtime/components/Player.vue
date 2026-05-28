1a3464d0e7f2a34c6774462db3bd09a64e88c232
xingxingmofash <xingxingmofashu@outlook.com>
2026-05-28 16:52:26 +0800

    chore: add project  dependencies

---
+++ packages/vue/runtime/components/Player.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+export interface PlayerProps {
+  as?: string
+}
+</script>
+<script lang="ts" setup>
+const props = withDefaults(defineProps<PlayerProps>(), {
+  as: 'div'
+})
+</script>
+<template>
+  <component :is="props.as">
+    <slot />
+  </component>
+</template>
